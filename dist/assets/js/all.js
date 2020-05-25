3a5f4d95cc191d0df7529312a878da15c456cbf3
hsin-yu <tina2793778@gmail.com>
2020-05-25 19:12:02 +0800

    修改 v1

--- dist/assets/js/all.js
+++ dist/assets/js/all.js
@@ -1,42 +1,23 @@
 "use strict";
 
 $(document).ready(function () {
-  $('.m-menu').click(function (e) {
+  $('.mobile-menu').click(function (e) {
     e.preventDefault();
     $('.menu').toggleClass('active');
-  }); //   // 隱藏所有的 li 內文
-  // $('.faq-card__item__body').hide();
-  // // // 預設讓第一個 li 加上 active
-  // // $('.faq-card__item .label').eq(0).addClass('active');
-  // // 預設讓第一個 li 內文 顯示
-  // $('.faq-card__item__body').eq(0).show();
-  // // 點擊 li
-  // $('.faq-card__item .label').click(function (e) {
-  //   // 取消預設 event 事件
-  //   e.preventDefault(); 
-  //   // 加上 active 屬性，並把其他有 active 的移除
-  //   // $(this).toggleClass('active').siblings().removeClass('active');
-  //   $(this).children:checked;
-  //   // 讓自己的 li body 切換收合
-  //   $(this).parent().next().slideToggle();
-  //   // 讓其他 li body 收合
-  //   $(this).siblings().parent().next().slideUp();
-  // })
-  //blog 手機版下拉選單
+  }); //blog下拉選單
 
-  $('.m-menu-blog').click(function (e) {
-    e.preventDefault();
-    $('.blog-menu-list').removeClass('close');
-    $('.blog-menu-list').toggleClass('active');
-    $('.m-menu-blog').hide();
-    $('.m-menu-blog-up').show();
-  }); //blog 手機版收回選單
+  $('.mobile-menu-blog').on('click', function () {
+    $('.mobile-menu-blog').toggleClass('active');
+    $('.blog-nav').toggleClass('active');
+    $('.js-blog-menu-list').toggleClass('active');
+  });
+  AOS.init({
+    offset: 400,
+    // offset (in px) from the original trigger point
+    delay: 0,
+    // values from 0 to 3000, with step 50ms
+    duration: 1000 // values from 0 to 3000, with step 50ms
 
-  $('.m-menu-blog-up').click(function (e) {
-    e.preventDefault();
-    $('.blog-menu-list').addClass('close').removeClass('active');
-    $('.m-menu-blog-up').hide();
-    $('.m-menu-blog').show();
   });
 });
 "use strict";
